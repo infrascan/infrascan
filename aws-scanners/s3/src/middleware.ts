dc2024de39ff07b73eb9ead13718f5cc9c2ee3e0
lfarrel6 <lfarrel6@tcd.ie>
2023-10-29 13:15:58 +0000

    chore(tests): update s3 scanner to have tests for error mapping middleware

--- aws-scanners/s3/src/middleware.ts
+++ aws-scanners/s3/src/middleware.ts
@@ -12,14 +12,55 @@ import {
   _Error,
 } from "@aws-sdk/client-s3";
 
+export type RichError = S3ServiceException & _Error;
+
 /**
  * The class for errors from S3 doesn't include the Code used to identify the Errors, unlike the public `_Error` interface does.
  * To safely ignore the known errors in `ignoreS3ConfigNotFoundErrors`, we need to assert that the error being thrown has the correct interface.
  */
 function isRichError(
   err: S3ServiceException & { Code?: string },
-): err is _Error & S3ServiceException {
+): err is RichError {
   return err.Code != null;
+}
+
+export function mapNotFoundErrorToEmptyResponse(
+  context: HandlerExecutionContext,
+  err: unknown,
+): FinalizeHandlerOutput<ServiceOutputTypes> | undefined {
+  const isRichS3Err = err instanceof S3ServiceException && isRichError(err);
+  if (!isRichS3Err) {
+    console.log("Not a rich error", err);
+    return undefined;
+  }
+
+  // Map NoSuchTagSet to empty TagSet
+  if (
+    context.commandName === "GetBucketTaggingCommand" &&
+    err.Code === "NoSuchTagSet"
+  ) {
+    return {
+      output: {
+        $metadata: {},
+        TagSet: [],
+      },
+      response: err,
+    };
+  }
+  // Map NoSuchWebsiteConfig to empty response
+  if (
+    context.commandName === "GetBucketWebsiteCommand" &&
+    err.Code === "NoSuchWebsiteConfiguration"
+  ) {
+    return {
+      output: {
+        $metadata: {},
+      },
+      response: err,
+    };
+  }
+
+  return undefined;
 }
 
 /**
@@ -28,7 +69,7 @@ function isRichError(
  * receive an empty object response than it is to error. This middleware will catch errors and, where appropriate,
  * return an empty object instead.
  */
-function ignoreS3ConfigNotFoundErrors(
+function ignoreS3ConfigNotFoundMiddleware(
   next: FinalizeHandler<ServiceInputTypes, ServiceOutputTypes>,
   context: HandlerExecutionContext,
 ): FinalizeHandler<ServiceInputTypes, ServiceOutputTypes> {
@@ -38,30 +79,9 @@ function ignoreS3ConfigNotFoundErrors(
     try {
       return await next(args);
     } catch (err: unknown) {
-      if (err instanceof S3ServiceException && isRichError(err)) {
-        if (
-          context.commandName === "GetBucketTaggingCommand" &&
-          err.Code === "NoSuchTagSet"
-        ) {
-          return {
-            output: {
-              $metadata: {},
-              TagSet: [],
-            },
-            response: err,
-          };
-        }
-        if (
-          context.commandName === "GetBucketWebsiteCommand" &&
-          err.Code === "NoSuchWebsiteConfiguration"
-        ) {
-          return {
-            output: {
-              $metadata: {},
-            },
-            response: err,
-          };
-        }
+      const mappedResponse = mapNotFoundErrorToEmptyResponse(context, err);
+      if (mappedResponse != null) {
+        return mappedResponse;
       }
       throw err;
     }
@@ -69,7 +89,7 @@ function ignoreS3ConfigNotFoundErrors(
 }
 
 export function registerMiddleware(client: S3Client) {
-  client.middlewareStack.add(ignoreS3ConfigNotFoundErrors, {
+  client.middlewareStack.add(ignoreS3ConfigNotFoundMiddleware, {
     step: "finalizeRequest",
     name: "ignoreS3ConfigNotFoundErrors",
     tags: ["ErrorHandling"],
