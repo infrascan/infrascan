d2fead177d5e5b892e6e33548835c9bdfe3d59f7
lfarrel6 <lfarrel6@tcd.ie>
2023-11-08 22:41:17 +0000

    chore(tests): add tests for internal cache in s3 connector

--- packages/s3-connector/src/cache.ts
+++ packages/s3-connector/src/cache.ts
@@ -9,6 +9,9 @@ export default class Cache<T> {
   keyOrders: Array<string>;
 
   constructor(maxSize: number) {
+    if (maxSize < 1) {
+      throw new Error("Cannot create a cache with a size less than 1");
+    }
     this.sizeLimit = maxSize;
     this.size = 0;
     this.state = {};
@@ -17,7 +20,7 @@ export default class Cache<T> {
 
   set(key: string, val: T) {
     if (this.size === this.sizeLimit) {
-      const keyToRemove = this.keyOrders.pop() as string;
+      const keyToRemove = this.keyOrders.shift() as string;
       delete this.state[keyToRemove];
       this.size -= 1;
     }
