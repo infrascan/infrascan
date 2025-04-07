803a576424409db3b7f35cbd60f10543c80f77b4
lfarrel6 <github@l3f7.dev>
2025-04-07 22:43:39 +0100

    chore: fix node-reducer plugin tests to comply with new data model

--- plugins/node-reducer/src/reducer.test.ts
+++ plugins/node-reducer/src/reducer.test.ts
@@ -1,4 +1,6 @@
 import { EdgeConflictError, Graph } from "@infrascan/core";
+import { BaseState, Edge, Node, Readable } from "@infrascan/shared-types";
+import t from "tap";
 import {
   addEdgeToGraphIfNotExists,
   aggregateByService,
@@ -7,23 +9,26 @@ import {
   ReducerRule,
 } from "./reducer";
 
-import t from "tap";
-import { Edge } from "@infrascan/shared-types";
-
 t.test(
   "AggregateByService reduces a list of items by the service key",
   (tap) => {
     const items = [
       {
-        service: "a",
+        resource: {
+          category: "a",
+        },
       },
       {
-        service: "a",
+        resource: {
+          category: "a",
+        },
       },
       {
-        service: "b",
+        resource: {
+          category: "b",
+        },
       },
-    ];
+    ] as unknown[] as BaseState[];
 
     const { a, b, c } = aggregateByService(items);
     tap.equal(a.length, 2);
@@ -41,12 +46,12 @@ t.test("applyRule regex rule with no match", (tap) => {
       service: "a",
     },
     {
-      id: "node-1",
-      name: "foobar",
-      metadata: {},
-      incomingEdges: {},
-      outgoingEdges: {},
-    },
+      $graph: {
+        id: "node-1",
+        label: "foobar",
+        nodeType: "foobar",
+      },
+    } as unknown as Readable<Node>,
   );
   tap.equal(matches, false);
   tap.end();
@@ -60,12 +65,12 @@ t.test("applyRule regex rule with match", (tap) => {
       service: "a",
     },
     {
-      id: "regex-1",
-      name: "foobar",
-      metadata: {},
-      incomingEdges: {},
-      outgoingEdges: {},
-    },
+      $graph: {
+        id: "regex-1",
+        label: "foobar",
+        nodeType: "foobar",
+      },
+    } as unknown as Readable<Node>,
   );
   tap.equal(matches, true);
   tap.end();
@@ -79,12 +84,12 @@ t.test("applyRule glob rule with no match", (tap) => {
       service: "a",
     },
     {
-      id: "elb-1",
-      name: "first elb",
-      metadata: {},
-      incomingEdges: {},
-      outgoingEdges: {},
-    },
+      $graph: {
+        id: "elb-1",
+        label: "first elb",
+        nodeType: "elb",
+      },
+    } as unknown as Readable<Node>,
   );
   tap.equal(matches, false);
   tap.end();
@@ -98,12 +103,12 @@ t.test("applyRule glob rule with match", (tap) => {
       service: "a",
     },
     {
-      id: "lambda-1",
-      name: "first lambda",
-      metadata: {},
-      incomingEdges: {},
-      outgoingEdges: {},
-    },
+      $graph: {
+        id: "lambda-1",
+        label: "first lambda",
+        nodeType: "lambda",
+      },
+    } as unknown as Readable<Node>,
   );
   tap.equal(matches, true);
   tap.end();
@@ -115,13 +120,13 @@ t.test("applyRule with invalid rule", (tap) => {
     id: "glob-test-1",
     service: "a",
   };
-  //@ts-expect-error
+  // @ts-expect-error Rule definition is invalid
   const result = applyRule(invalidRule, {
-    id: "lambda-1",
-    name: "first lambda",
-    metadata: {},
-    incomingEdges: {},
-    outgoingEdges: {},
+    $graph: {
+      id: "lambda-1",
+      label: "first lambda",
+      nodeType: "lambda",
+    },
   });
   tap.equal(result, false);
   tap.end();
@@ -133,7 +138,7 @@ t.test("addEdgeToGraphIfNotExists swallows conflict errors", (tap) => {
       throw new EdgeConflictError(edge.id);
     },
   };
-  //@ts-expect-error
+  // @ts-expect-error Partial graph implementation for test
   addEdgeToGraphIfNotExists(mockGraph, {});
   tap.pass();
   tap.end();
@@ -147,7 +152,7 @@ t.test("addEdgeToGraphIfNotExists swallows conflict errors", (tap) => {
     },
   };
   try {
-    //@ts-expect-error
+    // @ts-expect-error partial graph implementation for test
     addEdgeToGraphIfNotExists(mockGraph, {});
   } catch (err: unknown) {
     tap.equal(err, thrownErr);
@@ -172,43 +177,58 @@ t.test(
       reducedNodeSuffix: "serverless-buckets",
     };
 
-    const rules: ReducerRule[] = [lambdaRule, s3Rule];
     const graph = Graph();
 
     graph.addNode({
-      id: "account",
-      name: "account",
-      metadata: {},
-      service: "AWS-Account",
-    });
+      $graph: {
+        id: "account",
+        label: "account",
+        nodeType: "AWS-Account",
+      },
+      resource: {
+        category: "account",
+      },
+    } as unknown as BaseState<unknown>);
 
     for (let i = 0; i < 3; i++) {
       graph.addNode({
-        id: `function-${i}`,
-        name: `lambda func ${i}`,
-        metadata: {},
-        service: "lambda",
-        parent: "account",
-      });
+        $graph: {
+          id: `function-${i}`,
+          label: `lambda func ${i}`,
+          nodeType: "lambda",
+          parent: "account",
+        },
+        resource: {
+          category: "lambda",
+        },
+      } as unknown as BaseState<unknown>);
     }
 
     for (let i = 0; i < 3; i++) {
       graph.addNode({
-        id: `serverless-${i}`,
-        name: `sls bucket ${i}`,
-        metadata: {},
-        service: "s3",
-        parent: "account",
-      });
+        $graph: {
+          id: `serverless-${i}`,
+          label: `sls bucket ${i}`,
+          nodeType: "s3",
+          parent: "account",
+        },
+        resource: {
+          category: "s3",
+        },
+      } as unknown as BaseState<unknown>);
     }
 
     graph.addNode({
-      id: "ecs-task",
-      name: "ecs task",
-      metadata: {},
-      service: "ecs",
-      parent: "account",
-    });
+      $graph: {
+        id: "ecs-task",
+        label: "ecs task",
+        nodeType: "ecs",
+        parent: "account",
+      },
+      resource: {
+        category: "ecs",
+      },
+    } as unknown as BaseState<unknown>);
 
     graph.addEdge({
       metadata: {},
@@ -228,7 +248,11 @@ t.test(
       target: "serverless-0",
     });
 
-    reduceGraphWithRules(aggregateByService(rules), graph);
+    const ruleMap = {
+      [lambdaRule.service]: [lambdaRule],
+      [s3Rule.service]: [s3Rule],
+    };
+    reduceGraphWithRules(ruleMap, graph);
 
     // attempt to retrieve original nodes targetted by reducer
     for (let i = 0; i < 3; i++) {
@@ -245,16 +269,20 @@ t.test(
     const collapsedLambdaNode = graph.getNode(`account-${lambdaRule.id}`);
     tap.ok(collapsedLambdaNode);
     tap.ok(
-      collapsedLambdaNode?.incomingEdges[`ecs-task-${collapsedLambdaNode.id}`],
+      collapsedLambdaNode?.incomingEdges[
+        `ecs-task-${collapsedLambdaNode.$graph.id}`
+      ],
     );
 
     const collapsedS3Node = graph.getNode(`account-${s3Rule.id}`);
     tap.ok(collapsedS3Node);
-    tap.ok(collapsedS3Node?.incomingEdges[`ecs-task-${collapsedS3Node.id}`]);
+    tap.ok(
+      collapsedS3Node?.incomingEdges[`ecs-task-${collapsedS3Node.$graph.id}`],
+    );
 
     tap.ok(
       collapsedLambdaNode?.outgoingEdges[
-        `${collapsedLambdaNode.id}-${collapsedS3Node?.id}`
+        `${collapsedLambdaNode.$graph.id}-${collapsedS3Node?.$graph?.id}`
       ],
     );
 
